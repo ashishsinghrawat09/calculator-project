75e9c22afbc889c206292900998004e79eff8bdc
Ashish Rawat <ashishrawat542585@gmail.com>
2026-03-02 20:36:04 +0530

    Add files via upload

---
+++ Calculator.java
@@ -0,0 +1,47 @@
+ import java.util.Scanner;
+
+public class Calculator {
+
+    public static void main(String[] args) {
+
+        double num1, num2,num3;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter the numbers:");
+        num1 = sc.nextDouble();
+        num2 = sc.nextDouble();
+        num3=sc.nextDouble();
+
+        System.out.println("Enter the operator (+, -, *, /):");
+        char operator = sc.next().charAt(0);
+
+        double result;
+        switch (operator) {
+            case '+':
+                result = num1 + num2 +num3;
+                break;
+
+            case '-':
+                result = num1 - num2 - num3;
+                break;
+
+            case '*':
+                result = num1 * num2 *num3;
+                break;
+
+            case '/':
+                if (num2 == 0) {
+                    System.out.println("Division by zero is not allowed.");
+                    return;
+                }
+                result = num1 / num2/num3;
+                break;
+
+            default:
+                System.out.println("Invalid operator.");
+                return;
+        }
+        System.out.println("The final result:");
+        System.out.println(num1 + " " + operator + " " + num2 + " = " + result);
+    }
+}
